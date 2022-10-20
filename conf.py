498ba1463baa93bbf91b746115a6b2fe19a456d6
yokay <yokay886@qq.com>
2022-10-20 00:24:28 +0000

    update conf.py.
    Signed-off-by: yokay <yokay886@qq.com>

--- conf.py
+++ conf.py
@@ -185,20 +185,20 @@ latex_elements = {
     #'pointsize': '10pt',
     # Additional stuff for the LaTeX preamble.
     
-    \\hypersetup{unicode=true},
-    \\usepackage{CJKutf8},
-    \\DeclareUnicodeCharacter{00A0}{\nobreakspace},
-    \\DeclareUnicodeCharacter{2203}{\ensuremath{\exists}},
-    \\DeclareUnicodeCharacter{2200}{\ensuremath{\forall}},
-    \\DeclareUnicodeCharacter{2286}{\ensuremath{\subseteq}},
-    \\DeclareUnicodeCharacter{2713}{x},
-    \\DeclareUnicodeCharacter{27FA}{\ensuremath{\Longleftrightarrow}},
-    \\DeclareUnicodeCharacter{221A}{\ensuremath{\sqrt{}}},
-    \\DeclareUnicodeCharacter{221B}{\ensuremath{\sqrt[3]{}}},
-    \\DeclareUnicodeCharacter{2295}{\ensuremath{\oplus}},
-    \\DeclareUnicodeCharacter{2297}{\ensuremath{\otimes}},
-    \\begin{CJK}{UTF8}{gbsn},
-    \\AtEndDocument{\end{CJK}}
+    'preamble': \\hypersetup{unicode=true},
+    'preamble': \\usepackage{CJKutf8},
+    'preamble': \\DeclareUnicodeCharacter{00A0}{\nobreakspace},
+    'preamble': \\DeclareUnicodeCharacter{2203}{\ensuremath{\exists}},
+    'preamble': \\DeclareUnicodeCharacter{2200}{\ensuremath{\forall}},
+    'preamble': \\DeclareUnicodeCharacter{2286}{\ensuremath{\subseteq}},
+    'preamble': \\DeclareUnicodeCharacter{2713}{x},
+    'preamble': \\DeclareUnicodeCharacter{27FA}{\ensuremath{\Longleftrightarrow}},
+    'preamble': \\DeclareUnicodeCharacter{221A}{\ensuremath{\sqrt{}}},
+    'preamble': \\DeclareUnicodeCharacter{221B}{\ensuremath{\sqrt[3]{}}},
+    'preamble': \\DeclareUnicodeCharacter{2295}{\ensuremath{\oplus}},
+    'preamble': \\DeclareUnicodeCharacter{2297}{\ensuremath{\otimes}},
+    'preamble': \\begin{CJK}{UTF8}{gbsn},
+    'preamble': \\AtEndDocument{\end{CJK}}
     
     }
 # The font size ('10pt', '11pt' or '12pt').
