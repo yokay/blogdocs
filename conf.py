717c8d0c2296ae825235e5fef3c9a95f38571c37
yokay <525618861@qq.com>
2023-12-06 00:16:54 +0800

    V2.0

--- conf.py
+++ conf.py
@@ -113,14 +113,39 @@ html_theme = 'sphinx_material'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    # Set the name of the project to appear in the navigation.
+    'nav_title': 'Mythbird',
+
+    # Set you GA account ID to enable tracking
+    # 'google_analytics_account': 'UA-XXXXX',
+
+    # Specify a base_url used to generate sitemap.xml. If not
+    # specified, then no sitemap will be built.
+    'base_url': 'https://doc.mythbird.com',
+
+    # Set the color and the accent color
+    'color_primary': 'blue',
+    'color_accent': 'light-blue',
+
+    # Set the repo location to get a badge with stats
+    # 'repo_url': 'https://github.com/project/project/',
+    # 'repo_name': 'Project',
+
+    # Visible levels of the global TOC; -1 means unlimited
+    'globaltoc_depth': 3,
+    # If False, expand all TOC entries
+    'globaltoc_collapse': False,
+    # If True, show hidden TOC entries
+    'globaltoc_includehidden': False,
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = "Mythbird Blog"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
